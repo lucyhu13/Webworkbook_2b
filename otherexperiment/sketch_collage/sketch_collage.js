290376b30d94c56910db0eb584dd0830f73c31c9
ronghu <163358684+lucyhu13@users.noreply.github.com>
2024-04-16 00:21:45 +1000

    Add files via upload

---
+++ otherexperiment/sketch_collage/sketch_collage.js
@@ -0,0 +1,40 @@
+
+var img1, img2, img3, img4, img5, img6, img7, img8, img9, img10 ;
+
+function preload() {
+  img1 = loadImage('data/01.png');
+  img2 = loadImage('data/02.png');
+  img3 = loadImage('data/03.png');  
+  img4 = loadImage('data/04.png');
+  img5 = loadImage('data/05.png');
+  img6 = loadImage('data/06.png');  
+  img7 = loadImage('data/07.png');  
+ img8 = loadImage('data/08.png');
+  img9 = loadImage('data/09.png');  
+   img10 = loadImage('data/10.png');   
+}
+
+function setup() { 
+  createCanvas(windowWidth,windowHeight);}
+  
+
+
+  function draw(){}
+       function mousePressed(){
+ image(img1,random(width - img1.width),random(height - img1.height));
+ image(img2,random(width - img2.width),random(height - img2.height));
+ //image(img3,random(width - img3.width),random(height - img3.height),width/3);
+ //image(img4,random(width - img4.width),random(height - img4.height),width/3);
+ // image(img5,random(width - img5.width),random(height - img5.height),width/3);
+  // image(img6,random(width - img6.width),random(height - img6.height),width();
+   // image(img6,random(width - img7.width),random(height - img7.height),width/2);
+    //image(img8,random(width - img8.width),random(height - img8.height),width/3);
+//image(img9,random(width - img9.width),random(height - img9.height),width/5);
+//image(img10,random(100-1200),random(100-1200),width/2,height/2);
+
+
+
+
+
+ }
+ 
